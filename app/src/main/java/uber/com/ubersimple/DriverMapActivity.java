ae6ed7d9736db914d655be9209bfcd411ef3ae0e
Mohamed Ali Abd El_hakeem <mohamed.ali6996@hotmail.com>
2018-11-06 01:01:02 +0200

    geofire

--- app/src/main/java/uber/com/ubersimple/DriverMapActivity.java
+++ app/src/main/java/uber/com/ubersimple/DriverMapActivity.java
@@ -2,12 +2,13 @@ package uber.com.ubersimple;
 
 import android.annotation.SuppressLint;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.util.Log;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -18,15 +19,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DriverMapActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
     private LocationRequest mLocationRequest;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+    private DatabaseReference driversAvailableRef;
+    private String user_id;
+    private GeoFire geoFire;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,13 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        user_id = mCurrentUser.getUid();
+        driversAvailableRef = FirebaseDatabase.getInstance().getReference("driversAvailable");
+        geoFire = new GeoFire(driversAvailableRef);
+
     }
 
     @SuppressLint("MissingPermission")
@@ -70,7 +88,7 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
         mLocationRequest.setFastestInterval(1000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest, this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @Override
@@ -88,10 +106,25 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
         // called every second as set in location request
 
         mLastLocation = location;
-        LatLng latLng = new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude());
+        LatLng latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
 
+        geoFireDriverLocation(mLastLocation);
+
     }
 
+    private void geoFireDriverLocation(Location location) {
+        geoFire.setLocation(user_id, new GeoLocation(location.getLatitude(), location.getLongitude()));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // the driver is not available, remove his location
+        geoFire.removeLocation(user_id);
+
+
+
+    }
 }
